7b1586de157d9fd579fb38bd84e4b22a0ddc3f6a
AlfredBryan <alfred.chimereze@gmail.com>
2019-03-04 14:13:35 +0100

    remove consoles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -25,7 +25,6 @@ class Home extends Component {
           ...event
         };
       });
-      console.log(res.data);
       this.setState({
         events: updatedEvents,
         loading: false
